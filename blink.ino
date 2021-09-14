bd84223849d66d0f6bade21c36b7ffcad7c91079
amper <richard.benes.9@email.cz>
2021-09-14 08:18:45 +0200

    Added variable delay

--- blink.ino
+++ blink.ino
@@ -8,6 +8,7 @@
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
 int led = 13;
+unsigned int delay = 1000;
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -17,8 +18,12 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
+  if (delay >= 3000) {
+  	delay = 1000;
+  }  
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
+  delay(delay);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(2000);               // wait for a second
+  delay(delay);               // wait for a second
+  delay += 500;
 }
