d4f03dd8e5591f2d058b035d28b54a2d8e32ee29
amper <richard.benes.9@email.cz>
2021-09-20 06:45:15 +0200

    More magic by values

--- blink.ino
+++ blink.ino
@@ -8,7 +8,6 @@
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
 int led = 13;
-unsigned int delay = 3554;
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -19,7 +18,7 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(delay);               // wait for a second
+  delay(3554);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(delay);               // wait for a second
+  delay(3554);               // wait for a second
 }
