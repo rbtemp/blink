04999f0caa0e390c1dbfc493a168a9b2d86984f7
amper <richard.benes.9@email.cz>
2021-10-01 05:49:19 +0200

    More intensive code shortification

--- blink.ino
+++ blink.ino
@@ -1,15 +1,15 @@
-void setup() {                
-  pinMode(13, OUTPUT);     
+void setup(){
+pinMode(13, OUTPUT);     
 }
-void loop() {
-  digitalWrite(13, HIGH);
-  encapsulated_delay() 
-  digitalWrite(13, LOW);
-  encapsulated_delay2(); 
+void loop(){
+digitalWrite(13, HIGH);
+encapsulated_delay() 
+digitalWrite(13, LOW);
+encapsulated_delay2(); 
 }
-void encapsulated_delay() {
-  delay(3554);
+void encapsulated_delay(){
+delay(3554);
 }
-void encapsulated_delay2() {
-  delay(3554);
+void encapsulated_delay2(){
+delay(3554);
 }
