758066a683454d2ace736d4d12e2ca5e613408c3
amper <richard.benes.9@email.cz>
2021-10-01 05:45:00 +0200

    Comments gone

--- blink.ino
+++ blink.ino
@@ -1,21 +1,10 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-// the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
   pinMode(13, OUTPUT);     
 }
-// the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  encapsulated_delay();    
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(13, HIGH);
+  encapsulated_delay() 
+  digitalWrite(13, LOW);
   encapsulated_delay2(); 
 }
 void encapsulated_delay() {
