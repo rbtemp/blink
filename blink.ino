3ec70ee02b61598a2293de2cb3313bdfd7a6c9d8
amper <richard.benes.9@email.cz>
2021-10-01 05:28:57 +0200

    No spaces then

--- blink.ino
+++ blink.ino
@@ -4,16 +4,13 @@
  
   This example code is in the public domain.
  */
- 
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(13, OUTPUT);     
 }
-
 // the loop routine runs over and over again forever:
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
@@ -21,11 +18,9 @@ void loop() {
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   encapsulated_delay2(); 
 }
-
 void encapsulated_delay() {
   delay(3554);
 }
-
 void encapsulated_delay2() {
   delay(3554);
 }
